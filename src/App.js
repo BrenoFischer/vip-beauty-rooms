5b3836770fd221846c16c4ce9b6735a2f4a4feb7
Breno Fischer <breno_perricone@hotmai.com>
2023-03-09 10:15:42 -0300

    add firebase lib to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Details from './routes/details/details.component';
 import Home from './routes/home/home.component';
+import SignIn from './routes/signIn/signIn.component';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='details' element={<Details />} />
+      <Route path='sign-in' element={<SignIn />} />
     </Routes>
   );
 }
---
+++ src/routes/signIn/signIn.component.jsx
@@ -0,0 +1,9 @@
+import './signIn.styles.scss';
+
+const SignIn = () => {
+    return(
+        <h1>Sign In Page</h1>
+    );
+};
+
+export default SignIn;
