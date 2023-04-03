a247adf15cc86a2a0b2acad21f95ffd0f0bb0605
Breno Fischer <breno_perricone@hotmai.com>
2023-04-03 20:36:57 -0300

    add nav bar responsive design for smaller screens

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { useSelector } from 'react-redux';
 
 import { Link, Outlet } from 'react-router-dom';
@@ -10,6 +12,35 @@ import './navigation.styles.scss';
 const Navigation = () => {
     const currentUser = useSelector((state) => state.user.currentUser);
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    const LinksList = ({phoneLink=""}) => {
+        return(
+            <ul className='nav__links'>
+                <Link className={`nav__link ${phoneLink}`} onClick={() => { phoneLink && toggleMenu() }} to="posts/">Posts</Link>
+                <Link className={`nav__link ${phoneLink}`} onClick={() => { phoneLink && toggleMenu() }} to="services/">Services</Link>
+                { currentUser ? (
+                        <>
+                            <Link className={`nav__link ${phoneLink}`} onClick={() => { phoneLink && toggleMenu() }} to="add-post/">Add Post</Link>
+                            <Link className={`nav__link ${phoneLink}`} onClick={() => { phoneLink && toggleMenu() }} to="add-service/">Add Service</Link>
+                            <Link className={`nav__link ${phoneLink}`} onClick={() => { phoneLink && toggleMenu() }} to="edit-opening-hours/">Edit Opening Hours</Link>
+                            <span className={`nav__link ${phoneLink}`} onClick={signOutUser}>Sign Out</span>
+                        </>
+                    ) :
+                    (
+                        
+                        <Link className={`nav__link ${phoneLink}`} to="sign-in/">Sign In</Link>
+                    )
+
+                }
+            </ul>
+        )
+    }
+
     return(
         <>
             <nav className='nav'>
@@ -19,24 +50,33 @@ const Navigation = () => {
                     </Link>
                 </div>
                 <div className='nav__links-container'>
-                    <ul className='nav__links'>
-                        <Link className='nav__link' to="posts/">Posts</Link>
-                        <Link className='nav__link' to="services/">Services</Link>
-                        { currentUser ? (
-                                <>
-                                    <Link className='nav__link' to="add-service/">Add Service</Link>
-                                    <Link className='nav__link' to="edit-opening-hours/">Edit Opening Hours</Link>
-                                    <span className='nav__link' onClick={signOutUser}>Sign Out</span>
-                                </>
-                            ) :
-                            (
-                                
-                                <Link className='nav__link' to="sign-in/">Sign In</Link>
-                            )
-
+                    <div className='nav__links--desktop'>
+                        <LinksList />
+                    </div>
+                    <div className='nav__burger-menu' onClick={toggleMenu}>
+                        {menuOpen ?
+                            (<>
+                                <span className='close-bar'></span>
+                                <span className='close-bar-bottom'></span>
+                            </>)
+                        :
+                            (<>
+                                <span className='bar'></span>
+                                <span className='bar'></span>
+                                <span className='bar'></span>    
+                            </>)
                         }
-                    </ul>
+                    </div>
                 </div>
+                {menuOpen ?
+                    (<div className='nav__menu-open'>
+                        <ul className='nav__links-container--phone'>
+                            <LinksList phoneLink='nav__phone-link' />
+                        </ul> 
+                        </div>)
+                    :
+                        (<></>)
+                }
             </nav>
             <Outlet />
         </>
